454b82adc2d2127c239ec48f2a16af6e21632d7b
Germano Kuerten <germanokuerten@gmail.com>
2023-03-25 03:15:49 -0700

    fix: toggle menu _blank links added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -39,8 +39,8 @@ const Navbar = () => {
                         <li className='text-xl w-full my-2'>
                             <AiOutlineClose onClick={() => setToggleMenu(false)} />
                         </li>
-                        <li className='cursor-pointer py-2 px-5 mx-3 hover:shadow-[#109ee0] shadow-lg cursor-pointer my-2 text-lg'>GitHub</li>
-                <li className='cursor-pointer py-2 px-5 mx-3 hover:shadow-[#109ee0] shadow-lg cursor-pointer'>LinkedIn</li>
+                        <li className='cursor-pointer py-2 px-5 mx-3 hover:shadow-[#109ee0] shadow-lg cursor-pointer my-2 text-lg'><a href="https://github.com/germanokuerten/w3bxplr-frontend" target="_blank">GitHub</a></li>
+                        <li className='cursor-pointer py-2 px-5 mx-3 hover:shadow-[#109ee0] shadow-lg cursor-pointer'><a href="https://www.linkedin.com/in/germanokuerten/" target="_blank">LinkedIn</a></li>
                     </ul>
                 )}
             </div>

d4ae944ebb237568955c9e1b9b743b222f60a9ad
Germano Kuerten <germanokuerten@gmail.com>
2023-03-27 01:05:43 -0700

    Update: Code clean up

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -103,9 +103,7 @@ const Welcome = () => {
                             <button 
                                     type='button'
                                     className="cursor-pointer flex flex-row justify-center items-center my-5 py-2 pl-3 pr-3 rounded-r-md hover:shadow-[#109ee0] shadow-lg text-white border border-sky-500" 
-                                    
-                                    onClick={handleSearch}
-                                    
+                                    onClick={handleSearch}             
                                 >
                                     <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
                                     <p className='text-base font-semibold'></p>
